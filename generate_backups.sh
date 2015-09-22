bbb2a470503c4b40096567d4b4eb0a5d2dfe309c
Logan Marchione <loganmarchione@gmail.com>
2015-09-22 15:03:28 -0400

    Corrected variable

--- generate_backups.sh
+++ generate_backups.sh
@@ -17,6 +17,6 @@ rm /backup/backup/*.gz
 opkg list-installed | cut -f 1 -d ' ' > /etc/packages.txt
 
 #Backup /etc
-tar -czf ${BACKUP_FOLDER}/${HOSTNAME}_$(date +%Y%m%d_%H%M)_etc.tar.gz /etc >/dev/null 2>&1
+tar -czf ${BACKUP_FOLDER}/${HOSTNAME}_$(date +%Y%m%d_%H%M)_etc.tar.gz ${CONFIG_FOLDER} >/dev/null 2>&1
 
 exit
